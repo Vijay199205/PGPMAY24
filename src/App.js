b2103d6ec4ea432177abfe506b490ca6f6583f9b
Vijay199205 <vijayamogaveera100@gmail.com>
2024-12-08 08:10:53 +0530

    theme changes, state (useState)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react'
-import Childernprop from './Component/Childernprop';
-import Section from './Component/Section';
-//import Contact from './Csomponent/Contact';
-
-//var fname = 'vijay';
+import React, { useState } from 'react'
 
 function App() {
+  
+  const [condition, setCondition] = useState(false);
+  function handleTheme(){
+    setCondition(!condition);
+  }
   return (
-    <div>
-     <h1>Hello</h1>
-     {/* <Section rollNo="1" marks="85" subject="Physics">
-      <Childernprop/>
-     </Section> */}
-     {/* <Childernprop>
-      <h1>I am intersted to beacome full stack developer</h1>
-     </Childernprop>  */}
+    <div style={{backgroundColor:condition?"black":"white", color:condition?"white":"black"}}>
+    <h1>Hello Word</h1>
+    <p>Learning State</p>
+    <button onClick={handleTheme}>{condition?"Light":"white"} Mode</button>
 
-      {/* <Contact firstName={fname} age="32" salary="2000"/>
-      <Contact firstName="Vinay" age="40" salary="50000"/>
-      <Contact firstName="aditya" age="20" /> */}
+   
     </div>
   )
 }
